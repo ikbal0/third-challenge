e86544f5ede365d1f22fe1c4237acced38ed1833
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2023-03-14 16:22:57 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Create Input
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Type something: ")
+	scanner.Scan()
+
+	input := scanner.Text()
+
+	// create character container
+	var characterContainer = map[string]int{}
+
+	for i := range input {
+		var a string = string(byte(input[i]))
+		characterContainer[a] = 0
+		fmt.Printf("%v \n", a)
+	}
+
+	// count find and count each character
+	for i := range input {
+		var a string = string(byte(input[i]))
+		findCharacter(characterContainer, a)
+	}
+
+	fmt.Println("====================================================")
+	fmt.Println(characterContainer)
+}
+
+func findCharacter(characters map[string]int, a string) {
+	for key := range characters {
+		if key == a {
+			characters[key] += 1
+		}
+	}
+}
